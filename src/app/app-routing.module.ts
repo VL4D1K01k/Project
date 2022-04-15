997ee3a52397affa2341982fd1e7da01163d2d5c
vl4d1k01k <vlad.nicorici.16@gmail.com>
2022-04-15 12:26:21 +0300

    finish  router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
 import { AboutComponent } from "./about/about.component";
 import { AppPageComponent } from "./app-page/app-page.component";
 import { MainPageComponent } from "./main-page/main-page.component";
+import { RewardsPageComponent } from "./rewards-page/rewards-page.component";
 import { ShopPageComponent } from "./shop-page/shop-page.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: MainPageComponent },
     { path: 'app', component: AppPageComponent },
+    { path: 'rewards', component: RewardsPageComponent },
     { path: 'about', component: AboutComponent },
     { path: 'shop', component: ShopPageComponent },
 ]
